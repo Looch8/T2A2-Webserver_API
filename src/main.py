c6b1b6a57f044861e05b49e73ab04136800d2f65
Looch8 <lukewheldale@gmail.com>
2023-07-20 10:47:16 +0930

    Set up venv, and init files

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import os
+from init import db, ma, bcrypt, jwt
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "DATABASE_URL")  # This is the database URL, it is in the .env file
+
+    # This is a secret key, it is in the .env file and it is used to encrypt the JWT token
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    return app
