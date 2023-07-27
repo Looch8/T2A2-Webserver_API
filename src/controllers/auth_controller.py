aba77f67ccf7a728067e8a8074cbc91932642031
Looch8 <lukewheldale@gmail.com>
2023-07-27 16:40:44 +0930

    Added Login Route

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -3,14 +3,19 @@ from init import db, bcrypt
 from models.applicant import Applicant, applicant_schema, applicants_schema
 from flask_jwt_extended import create_access_token
 from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
+from datetime import timedelta
 
 # the auth_controller.py file is used to create the auth blueprint and to create the routes for the auth blueprint.
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 # this endpoint is used to login applicants.
 
+# Registration route
+
 
 @auth_bp.route("/register", methods=["POST"])
+# this function is used to register applicants.
 def auth_register():
     try:
         body_data = request.get_json()
@@ -38,3 +43,24 @@ def auth_register():
             # returns the error message and the column name that caused the error.
             return {"error": f" the {e.orig.diag.column_name} is required"}, 401
     # 401 is the status code for unauthorized
+
+# Login Route
+
+
+@auth_bp.route("/login", methods=["POST"])
+# this function is used to login applicants.
+def auth_login():
+    body_data = request.get_json()
+    # Find user by email
+    stmt = db.select(Applicant).where(
+        Applicant.email == body_data.get("email"))
+    applicant = db.session.scalar(stmt)
+    # If user exists, and password matches, create access token
+    if applicant and bcrypt.check_password_hash(applicant.password, body_data.get("password")):
+        token = create_access_token(identity=str(
+            applicant.id), expires_delta=timedelta(days=1))
+        # 200 is the status code for OK
+        return {'email': applicant.email, 'token': token, 'is_admin': applicant.is_admin}, 200
+    else:
+        # 401 is the status code for unauthorized
+        return {"error": "Invalid email or password"}, 401
