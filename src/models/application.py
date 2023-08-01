95906c6de4c8a827009e4cd7eb57ca7ae42f9a4d
Looch8 <lukewheldale@gmail.com>
2023-08-01 19:20:09 +0930

    added relationship between applicant and applications models

--- src/models/application.py
+++ src/models/application.py
@@ -8,23 +8,26 @@ class Application(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_applied = db.Column(db.Date, nullable=False)
 
-    job_id = db.Column(db.Integer, db.ForeignKey("jobs.id"))
-    applicant_id = db.Column(db.Integer, db.ForeignKey("applicants.id"))
-    status_id = db.Column(db.Integer, db.ForeignKey("statuses.id"))
+    job_id = db.Column(db.Integer, db.ForeignKey("jobs.id"), nullable=False)
+    applicant_id = db.Column(db.Integer, db.ForeignKey(
+        "applicants.id"), nullable=False)
+    status_id = db.Column(db.Integer, db.ForeignKey(
+        "statuses.id"), nullable=False)
 
-    # The back_populates argument is used to create a relationship between the Job, Applicant, and Status model and the Company model.
+    # The back_populates argument is used to create a relationship between the Job, Applicant, and Status model with the application model.
     job = db.relationship("Job", back_populates="applications")
     applicant = db.relationship("Applicant", back_populates="applications")
     status = db.relationship("Status", back_populates="applications")
 
 
 class ApplicationSchema(ma.Schema):
-    job = fields.Nested("JobSchema", only=["id", "title"])
-    applicant = fields.Nested("ApplicantSchema", only=["id", "name"])
-    status = fields.Nested("StatusSchema", only=["id", "offer_status"])
+    job = fields.Nested("JobSchema", only=["title"])
+    applicant = fields.Nested("ApplicantSchema", only=["name", "email"])
+    status = fields.Nested("StatusSchema", only=["offer_status"])
 
     class Meta:
-        fields = ("id", "date_applied", "job_id", "applicant_id", "status_id")
+        # The fields are used to specify the fields that will be serialized.
+        fields = ("id", "date_applied", "job", "applicant", "status")
         ordered = True  # this line is used to order the fields in the schema.
 
 
